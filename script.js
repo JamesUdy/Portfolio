a107f920abc33795270a38267cb719805c64d5f2
James Udy <109093340+JamesUdy@users.noreply.github.com>
2023-02-23 01:14:32 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Web Developer","Java Developer", "Programmer", "Designer"],
+        strings: ["Web Developer", "Software Developer", "Data Analyst", "Programmer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Web Developer","Java Developer", "Programmer", "Designer"],
+        strings: ["Web Developer", "Software Developer", "Data Analyst", "Programmer", "Designer"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
